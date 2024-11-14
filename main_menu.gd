9abe4d376e37e362691592213cb0e9028f45ba69
Jonas64 <jonaslynnebakken@gmail.com>
2024-11-14 09:13:36 +0100

    Quit button function

--- main_menu.gd
+++ main_menu.gd
@@ -2,8 +2,12 @@ extends Control  # Or whatever your root node type is
 
 func _ready():
 	# Connect the TextureButton's "pressed" signal to the _on_StartButton_pressed function
-	$VBoxContainer/TextureButton.pressed.connect(_on_StartButton_pressed)
+	$start_btn/TextureButton.pressed.connect(_on_StartButton_pressed)
+	$quit_btn/TextureButton.pressed.connect(_on_QuitButton_pressed)
 
 func _on_StartButton_pressed():
 	# Change the scene to world.tscn
 	get_tree().change_scene_to_file("res://world.tscn")
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
