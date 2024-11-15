a4782e368dcf39634ce91be036a7469086614b10
Jonas64 <jonaslynnebakken@gmail.com>
2024-11-15 12:47:23 +0100

    net collision

--- net.gd
+++ net.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Area2D
 
 var rng = RandomNumberGenerator.new()
 
@@ -19,3 +19,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	get_tree().change_scene_to_file("res://main_menu.tscn")
